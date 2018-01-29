f5950c0ef85eb4a5aeea0b1748fb5578e0f26868
anis.tajouri <anis.tajouri@gmail.com>
2018-01-29 09:23:58 +0100

    first commit

---
+++ webapi/Utils/MailPictureManager.py
@@ -0,0 +1,71 @@
+# Import smtplib for the actual sending function
+import smtplib
+ 
+# For guessing MIME type
+import mimetypes
+ 
+# Import the email modules we'll need
+import email
+import email.mime.application
+ 
+#Import sys to deal with command line arguments
+import sys
+
+
+class MailPictureManager(object):
+    @classmethod
+    def email_photo(cls):
+          print("Hello: camera")
+#         # Create a text/plain message
+#         msg = email.mime.Multipart.MIMEMultipart()
+#         msg['Subject'] = 'Alerte tapis'
+#         msg['From'] = 'anis.tajouri@gmail.com'
+#         msg['To'] = 'anis.tajouri@gmail.com'
+
+         
+#         # The main body is just another attachment
+#         body = email.mime.Text.MIMEText("""Bonjour un""")
+#         msg.attach(body)
+         
+#         # PDF attachment block code
+#         # Nico: Ok, use a fixed image...and a jpg not pdf
+
+
+# # ts=`date`
+# # raspistill [opts] -o puppy.jpg
+# # echo "Our puppy $ts" | mail -a puppy.jpg -s PuppyCam your@email.com         
+#         #directory=sys.argv[1]
+#         directory = 'photo.jpg'
+         
+#         # Split de directory into fields separated by / to substract filename
+         
+#         spl_dir=directory.split('/')
+         
+#         # We attach the name of the file to filename by taking the last
+#         # position of the fragmented string, which is, indeed, the name
+#         # of the file we've selected
+         
+#         filename=spl_dir[len(spl_dir)-1]
+         
+#         # We'll do the same but this time to extract the file format (pdf, epub, docx...)
+         
+#         spl_type=directory.split('.')
+         
+#         type=spl_type[len(spl_type)-1]
+         
+#         fp=open(directory,'rb')
+#         att = email.mime.application.MIMEApplication(fp.read(),_subtype=type)
+#         fp.close()
+#         att.add_header('Content-Disposition','attachment',filename=filename)
+#         msg.attach(att)
+         
+#         # send via Gmail server
+#         # NOTE: my ISP, Centurylink, seems to be automatically rewriting
+#         # port 25 packets to be port 587 and it is trashing port 587 packets.
+#         # So, I use the default port 25, but I authenticate.
+#         s = smtplib.SMTP('smtp.gmail.com:587')
+#         s.starttls()
+#         s.login('anis.tajouri@gmail.com','Fitality119')
+#         s.sendmail('anis.tajouri@gmail.com','anis.tajouri@sofrecom.com', msg.as_string())
+#         s.quit()
+
---
+++ webapi/views/MailPictureView.py
@@ -0,0 +1,28 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from webapi.Utils.MailPictureManager import MailPictureManager
+from webapi.serializers.MailPictureManagerSerializer import MailPictureManagerSerializer
+
+class MailPictureManagement(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        """
+        Get the mail_address
+        """
+        print("HHHHHHHHHHHHHHHHHHHHHHHHH")
+        MailPictureManager.email_photo()       
+#        content = {'mail_address': MailPictureManager.get_mail_address()}
+        content = {'mail_address': 'anis.tajouri@gmail.com'}
+        return Response(content)
+
+    # def post(self, request):
+    #     serializer = MailPictureManagerSerializer(data=request.data)
+
+    #     if serializer.is_valid():
+    #         MailPictureManager.set_picture_nbr(serializer.validated_data["mail_address"])
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
