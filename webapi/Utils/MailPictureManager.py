c992fb8a9457cfa6d13403ae45a123ebf2e36ef1
anis.tajouri <anis.tajouri@gmail.com>
2018-01-29 11:51:36 +0100

    Mail another method
    Signed-off-by: anis.tajouri <anis.tajouri@gmail.com>

--- webapi/Utils/MailPictureManager.py
+++ webapi/Utils/MailPictureManager.py
@@ -12,9 +12,39 @@ import email.mime.application
 import sys
 
 
+import smtplib
+ 
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver='smtp.gmail.com:587'):
+    header  = 'From: %s\n' % from_addr
+    header += 'To: %s\n' % ','.join(to_addr_list)
+    header += 'Cc: %s\n' % ','.join(cc_addr_list)
+    header += 'Subject: %s\n\n' % subject
+    message = header + message
+ 
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+    return problems
+
+
+
 class MailPictureManager(object):
     @classmethod
     def email_photo(cls):
+
+          sendemail(from_addr    = 'anis.tajouri@gmail.com', 
+                  to_addr_list = ['anis.tajouri@gmail.com'],
+                  cc_addr_list = ['anis.tajouri@sofrecom.com'], 
+                  subject      = 'Howdy', 
+                  message      = 'Howdy from a python function', 
+                  login        = 'AnisTajouri', 
+                  password     = 'Fitality119')
+
           print("Hello: camera")
 #         # Create a text/plain message
 #         msg = email.mime.Multipart.MIMEMultipart()
